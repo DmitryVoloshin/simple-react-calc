cb3ccf08f7f8ce8d9701e77c3dd6b7b6bb9f16ef
Dmitriy Voloshin <voloshincode@gmail.com>
2021-03-17 16:31:58 +0200

    some colors for buttons

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -2,8 +2,24 @@ import React from 'react';
 
 import './Buttons.css'
 
-const Buttons = (  {content}  ) =>{
-    return <button className="Main-calc_button">{content}</button>
+const Buttons = (  {content,color}  ) =>{
+    //checking Zero
+    if(content == "0" || content == "zero"){
+        return <button className="Main-calc_button zero"
+        >{content}</button>
+    //Checking Color
+    }else if(color == "orange"){
+        return <button className="Main-calc_button orange"
+        >{content}</button>
+    }else if(color == "gray"){
+        return <button className="Main-calc_button gray"
+        >{content}</button>
+    // Just rend
+    }else{
+        return <button className="Main-calc_button"
+        >{content}</button>
+    }
+ 
 }
 
 export default Buttons
--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -12,8 +12,37 @@ const Calculator = ( ) =>{
 
             <div className="Calc-disp_buttons">
             
-                <Buttons content="AC"/>
+                <Buttons content="AC" color="gray"/>
+                <Buttons content="+/−"color="gray"/>
+                <Buttons content="%" color="gray"/>
+                <Buttons content="÷" color="orange"/>
+
+                <Buttons content="mc"/>
+                <Buttons content="mr"/>
+                <Buttons content="m-"/>
+                <Buttons content="m+" color="orange"/>
+
+                <Buttons content="7"/>
+                <Buttons content="8"/>
+                <Buttons content="9"/>
+                <Buttons content="×" color="orange"/>
+
+                <Buttons content="4"/>
+                <Buttons content="5"/>
+                <Buttons content="6"/>
+                <Buttons content="-" color="orange"/>
+
+                <Buttons content="1"/>
+                <Buttons content="2"/>
+                <Buttons content="3"/>
+                <Buttons content="+" color="orange"/>
+
+                <Buttons content="0"/>
+                <Buttons content=","/>
+                <Buttons content="=" color="orange"/>
             </div>
+
+            <div>slash here</div>
         </div>
     )
 }
