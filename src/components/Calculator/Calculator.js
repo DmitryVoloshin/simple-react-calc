d1cf9e01ab9cb1843a3bb66df5a01d2a29b1c50c
Dmitriy Voloshin <voloshincode@gmail.com>
2021-03-17 18:54:31 +0200

    simple operation is ready

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -4,83 +4,142 @@ import Buttons from '../Buttons';
 import './Calculator.css'
 
 
-const Calculator = ( ) =>{
+const Calculator = (  ) =>{
 
-    const [result,setResult] = useState('0')
-    const [store,setStore] = useState(null)
-    const [operator,setOperator] = useState(null)
+
+    const [result, setResult] = useState("0");
+    const [store, setStore] = useState(null);
+    const [operator, setOperator] = useState(null);
+    const [memory,setMemory] = useState("0")
 
     const onButtonPress = (content) => ()=>{
-        const numb = parseFloat(result)
+        const num = parseFloat(result);
 
-        if(content == "AC"){
-            setResult('0');
+        if( content === "mc"){
+            console.log('memory saved')
+        }
+        if (content === "AC") {
+            setResult("0");
             setStore(null);
-            setOperator(null)
-            return;
-        }
-        if(content == "+/−"){
-            setResult(( numb * -1).toString());
-            return;
-        }
-        if( content == "%"){
-            setResult((numb / 100).toString());
-            setStore(null);
-            setOperator(null);
-            return;
-        }
-
-       
-        if(content == "+"){
-            setStore(parseFloat(result));
-            setResult('0');
-            setOperator("+")
-            return;
-        }
-        if(content == "-"){
-            setStore(parseFloat(result));
-            setResult('0')
-            setOperator("-")
-            return;
-        }
-        if(content == "÷"){
-            setStore(parseFloat(result));
-            setResult('0')
-            setOperator("÷")
-            return;
-        }
-        if(content == "×"){
-            setStore(parseFloat(result))
-            setResult('0')
-            setOperator("×")
-            return;
+          setOperator(null);
+          return;
         }
     
-        if(content == "="){
-            if(!operator) return;
-
-            if(operator == "+"){
-                setResult((store + parseFloat(result)).toString())
-            }else if(operator == "-"){
-                setResult((store - parseFloat(result)).toString())
-            }else if(operator == "÷"){
-                setResult((store / parseFloat(result)).toString())
-            }else if(operator == "×"){
-                setResult((store * parseFloat(result)).toString())
-            }
-            setStore(null);
-            setOperator(null)
-            return
+        if (content === "+/−") {
+          setResult((num * -1).toString());
+          return;
         }
-        
+    
+        if (content === "%") {
+            setResult((num / 100).toString());
+            setStore(null);
+          setOperator(null);
+          return;
+        }
+    
+        if (content === ".") {
+          if (result.includes(".")) return;
+    
+          setResult(result + ".");
+          return;
+        }
+    
+        if (content === "+") {
+          if (operator !== null) {
+            if (operator === "+") {
+                setStore(store + parseFloat(result));
+            } else if (operator === "−") {
+                setStore(store - parseFloat(result));
+            } else if (operator === "×") {
+                setStore(store * parseFloat(result));
+            } else if (operator === "÷") {
+                setStore(store / parseFloat(result));
+            }
+          } else {
+            setStore(parseFloat(result));
+          }
+          setResult("0");
+          setOperator("+");
+          return;
+        }
 
-
-        
-
-
-
-        setResult((parseFloat(numb + content)).toString());
-
+        if (content === "−") {
+          if (operator !== null) {
+            if (operator === "+") {
+                setStore(store + parseFloat(result));
+            } else if (operator === "−") {
+                setStore(store - parseFloat(result));
+            } else if (operator === "×") {
+                setStore(store * parseFloat(result));
+            } else if (operator === "÷") {
+                setStore(store / parseFloat(result));
+            }
+          } else {
+            setStore(parseFloat(result));
+          }
+          setResult("0");
+          setOperator("−");
+          return;
+        }
+        if (content === "×") {
+          if (operator !== null) {
+            if (operator === "+") {
+                setStore(store + parseFloat(result));
+            } else if (operator === "−") {
+                setStore(store - parseFloat(result));
+            } else if (operator === "×") {
+                setStore(store * parseFloat(result));
+            } else if (operator === "÷") {
+                setStore(store / parseFloat(result));
+            }
+          } else {
+            setStore(parseFloat(result));
+          }
+          setResult("0");
+          setOperator("×");
+          return;
+        }
+        if (content === "÷") {
+          if (operator !== null) {
+            if (operator === "+") {
+                setStore(store + parseFloat(result));
+            } else if (operator === "−") {
+                setStore(store - parseFloat(result));
+            } else if (operator === "×") {
+                setStore(store * parseFloat(result));
+            } else if (operator === "÷") {
+                setStore(store / parseFloat(result));
+            }
+          } else {
+            setStore(parseFloat(result));
+          }
+          setResult("0");
+          setOperator("÷");
+          return;
+        }
+    
+        if (content === "=") {
+          if (!operator) return;
+    
+          if (operator === "+") {
+            setResult((store + parseFloat(result)).toString());
+          } else if (operator === "−") {
+            setResult((store - parseFloat(result)).toString());
+          } else if (operator === "×") {
+            setResult((store * parseFloat(result)).toString());
+          } else if (operator === "÷") {
+            setResult((store / parseFloat(result)).toString());
+          }
+          setStore(null);
+          setOperator(null);
+          return;
+        }
+    
+        if (result[result.length - 1] === ".") {
+        setResult(result + content);
+        } else {
+        setResult(parseFloat(num + content).toString());
+        }
     };
 
 
@@ -109,7 +168,7 @@ const Calculator = ( ) =>{
                 <Buttons onButtonClick={onButtonPress} content="4"/>
                 <Buttons onButtonClick={onButtonPress} content="5"/>
                 <Buttons onButtonClick={onButtonPress} content="6"/>
-                <Buttons onButtonClick={onButtonPress} content="-" color="orange"/>
+                <Buttons onButtonClick={onButtonPress} content="−" color="orange"/>
 
                 <Buttons onButtonClick={onButtonPress} content="1"/>
                 <Buttons onButtonClick={onButtonPress} content="2"/>
