34ce142ec4fda9f1286b81e6bf6ded9aefe01905
Dmitriy Voloshin <voloshincode@gmail.com>
2021-03-17 15:50:17 +0200

    new component calculator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Calculator from './components/Calculator/Calculator';
 
 function App() {
   return (
     <div className="App">
-        Hi
+        <Calculator/>
     </div>
   );
 }
---
+++ src/components/Calculator/Calculator.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './Calculator.css'
+
+
+const Calculator = ( ) =>{
+    return(
+        <div className="Calc-main_disp">
+            
+            <div className="Calc-disp_result">0</div>
+
+            <div className="Calc-disp_buttons">
+                <button>1</button>
+            </div>
+        </div>
+    )
+}
+
+export default Calculator
